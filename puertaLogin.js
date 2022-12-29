3e6c0b5a12ff3b6c0fb5df27218ee992ef30442c
JorgeFernandezRojoAndres <120122104+JorgeFernandezRojoAndres@users.noreply.github.com>
2022-12-29 09:09:45 -0300

    Add files via upload

---
+++ puertaLogin.js
@@ -0,0 +1,16 @@
+
+      
+      // El código JavaScript va aquí
+      const loginButton = document.getElementById("login-button");
+      const imageUploader = document.getElementById("image-uploader");
+      const currentPoints = document.getElementById("current-points");
+
+      loginButton.addEventListener("click", () => {
+        // Código para iniciar sesión con YouTube va aquí
+      });
+
+      imageUploader.addEventListener("change", () => {
+        // Código para subir imágenes va aquí
+        // Esto también debería actualizar el total de puntos del usuario
+      });
+    
